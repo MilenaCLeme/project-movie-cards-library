fec907625526206a65f3e8ab996fcaf6732e789f
MilenaCLeme <milenaleme4@hotmail.com>
2021-12-16 00:00:59 -0300

    adicionando className para passar do test

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,7 +5,9 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <p>{ rating }</p>
+      <div className="movie-card-rating">
+        <p className="rating">{ rating }</p>
+      </div>
     );
   }
 }
